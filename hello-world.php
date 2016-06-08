7e659d7cec8244fe5e92c3779ff00931fe98dc2e
NOEL S. DAVID <noelsdavid@gmail.com>
2016-06-08 23:03:04 +0000

    good morning

--- hello-world.php
+++ hello-world.php
@@ -4,9 +4,9 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
-echo 'Hello from NB!';
-echo 'Hello again from C9';
+echo 'Hello world from Cloud9!'. '<br>';
+echo 'Hello from NB!'. '<br>';
+echo 'Hello again from C9'. '<br>';
 
 ?>
 </body>
